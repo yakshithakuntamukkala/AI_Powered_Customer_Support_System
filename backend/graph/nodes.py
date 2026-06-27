e7b08570f1d62b5f5e51978a1e7822e82315a63b
yakshithakuntamukkala <yakshithakuntamukkala@gmail.com>
2026-06-27 11:17:02 +0000

    Implement intent classification node

--- backend/graph/nodes.py
+++ backend/graph/nodes.py
@@ -2,11 +2,32 @@ from graph.state import CustomerSupportState
 
 
 def receive_customer_query(state: CustomerSupportState):
+    print(f"\nCustomer Query: {state['query']}")
+    return state
+
+
+def classify_intent(state: CustomerSupportState):
     """
-    Entry node of the graph.
-    Receives the customer's query.
+    Classify the customer's query into a department.
     """
 
-    print("Customer Query:", state["query"])
+    query = state["query"].lower()
+
+    if any(word in query for word in ["price", "pricing", "plan", "subscription"]):
+        state["intent"] = "Sales"
+
+    elif any(word in query for word in ["error", "bug", "login", "crash", "install", "configuration"]):
+        state["intent"] = "Technical"
+
+    elif any(word in query for word in ["invoice", "payment", "refund", "billing"]):
+        state["intent"] = "Billing"
+
+    elif any(word in query for word in ["password", "profile", "account", "activate", "deactivate"]):
+        state["intent"] = "Account"
+
+    else:
+        state["intent"] = "Unknown"
+
+    print(f"Detected Intent: {state['intent']}")
 
     return state
--- backend/graph/workflow.py
+++ backend/graph/workflow.py
@@ -1,14 +1,18 @@
 from langgraph.graph import StateGraph, START, END
 
 from graph.state import CustomerSupportState
-from graph.nodes import receive_customer_query
-
+from graph.nodes import (
+    receive_customer_query,
+    classify_intent,
+)
 
 builder = StateGraph(CustomerSupportState)
 
 builder.add_node("receive_query", receive_customer_query)
+builder.add_node("intent_classifier", classify_intent)
 
 builder.add_edge(START, "receive_query")
-builder.add_edge("receive_query", END)
+builder.add_edge("receive_query", "intent_classifier")
+builder.add_edge("intent_classifier", END)
 
 graph = builder.compile()
